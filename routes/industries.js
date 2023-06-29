e521d52dbeff5a2b9a8b4109990c74bd2a993574
Hannes Porombka <hapo@Lutz-MacBook-pro-15.local>
2023-06-29 17:50:54 +0200

    added industries_companies route

--- routes/industries.js
+++ routes/industries.js
@@ -3,6 +3,7 @@ const router = new express.Router();
 const db = require('../db')
 const ExpressError = require("../expressError");
 router.use(express.json())
+const slug = require("slugify")
 
 
 router.get("/", async function (req, res) {
@@ -23,7 +24,54 @@ router.get("/", async function (req, res) {
     return res.json({ industries: results1.rows });
 });
 
+router.post("/", async function (req, res, next) {
+    try {
+        if (req.body.length == 0 || !req.body) {
+            throw new ExpressError("Please enter data", 400)
+        }
+        const { code, industry} = req.body
 
+        const slugcode = slug(code,{remove: /[*+~.()'"$!:@]/, replacement: '_', strict: true, lower: true, trim: true})
+
+        if (!code || !industry) {
+            throw new ExpressError("Pleaser enter Data in right format", 422)
+        }
+
+        const result = await db.query("INSERT INTO industries (code, industry) VALUES ($1, $2) RETURNING code, industry", [slugcode, industry])
+        if (result.length == 0){
+            return res.status(400).json("Sorry this industry is already in the DB")
+        }
+        return res.status(201).json(result.rows[0]);
+    }
+    catch (e) {
+        next(e)
+    }
+});
+
+
+router.post("/companies", async function (req, res, next) {
+    try {
+        if (req.body.length == 0 || !req.body) {
+            throw new ExpressError("Please enter data", 400)
+        }
+        const { comp_code, ind_code} = req.body
+
+       
+
+        if (!comp_code || !ind_code) {
+            throw new ExpressError("Pleaser enter Data in right format", 422)
+        }
+
+        const result = await db.query("INSERT INTO industries_companies (comp_code, ind_code) VALUES ($1, $2) RETURNING comp_code, ind_code", [comp_code, ind_code])
+        if (result.length == 0){
+            return res.status(400).json("Sorry this company/industry relation is already in the DB")
+        }
+        return res.status(201).json(result.rows[0]);
+    }
+    catch (e) {
+        next(e)
+    }
+});
 
 module.exports = router;
 
